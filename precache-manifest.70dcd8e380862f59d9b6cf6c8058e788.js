e495387694472e068e9dfa7e3b40779c5b7211cb
Ashish Agrawat <akagrawat@gmail.com>
2020-04-03 12:21:06 +0530

    Updates

---
+++ precache-manifest.70dcd8e380862f59d9b6cf6c8058e788.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cd436d49445d5da9714e3df89dc4937",
+    "url": "/React-validation-form/index.html"
+  },
+  {
+    "revision": "efb22b03c3892a67fb02",
+    "url": "/React-validation-form/static/css/main.508acd2d.chunk.css"
+  },
+  {
+    "revision": "48631ede2b085e71aad2",
+    "url": "/React-validation-form/static/js/2.3347e529.chunk.js"
+  },
+  {
+    "revision": "efb22b03c3892a67fb02",
+    "url": "/React-validation-form/static/js/main.1a92d621.chunk.js"
+  },
+  {
+    "revision": "e3c93730346e4d84f65f",
+    "url": "/React-validation-form/static/js/runtime~main.1407abca.js"
+  },
+  {
+    "revision": "4628c579c31e17ef5bca0d027c66d6fd",
+    "url": "/React-validation-form/static/media/bg-heading-02.4628c579.jpg"
+  }
+]);
